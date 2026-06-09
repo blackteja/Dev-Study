4039de334addad3190bd571cbfdf682174289647
Charlie <blackteja1@gmail.com>
2026-06-09 13:30:31 +0900

    Git Public Repository를 Study 용으로 만들었기에 첫 커밋을 위한 C언어 예제 작성

---
+++ C/CH00_ReStart_Resolution/Ex00_NewMindSet/NewMindSet.c
@@ -0,0 +1,17 @@
+﻿/*
+ * 2026년 6월 9일 화요일 13시 24분
+ * C언어 및 다른 언어 공부를 할 때 스스로 예제를 활용하여 나만의 예제를 제작하고
+ * Git Public Repository에 업로드 할 수 있도록 폴더를 새로 만들어서 비슷한 예제를 작성
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  // 이전에 로컬에 작성한 적 있는 예제이기 때문에 가장 간단히 Git Upload Test 용도로
+  // 널리 쓰이는 Hello World! 를 작성한다.
+  printf("Hello World!\n");
+  printf("이제 유혹에 흔들리지 않고 꾸준히 노력해서 위대한 개발자가 되자!");
+
+  return 0;
+}
